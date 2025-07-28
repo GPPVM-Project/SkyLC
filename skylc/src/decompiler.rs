98e8165717c682bc49dcad9b52e21cf230e56204
Vitorhenriquesilvasdesa <vitorhenriquesilvadesa@gmail.com>
2025-07-28 00:08:31 -0300

    Fix: removing some warnings of rustc.

--- skylc/src/decompiler.rs
+++ skylc/src/decompiler.rs
@@ -9,22 +9,21 @@ impl Decompiler {
         }
 
         for (descriptor, info) in &code.methods {
-            Decompiler::decompile_methods(descriptor, info, &code);
+            Decompiler::decompile_methods(descriptor, info, code);
         }
 
         for (name, function) in &code.functions {
             let width = 60;
-            println!(
-                "{}",
+
+            let fmt_fn = format!(
+                "{:=^1$}",
                 format!(
-                    "{:=^1$}",
-                    format!(
-                        " {} (id = {}; arity = {}) ",
-                        name, function.id, function.arity
-                    ),
-                    width
-                )
+                    " {} (id = {}; arity = {}) ",
+                    name, function.id, function.arity
+                ),
+                width
             );
+            println!("{fmt_fn}");
             Decompiler::decompile_function(function, code);
             println!("{}", "=".repeat(width));
             println!("\n");
@@ -51,129 +50,126 @@ impl Decompiler {
     fn decompile_instruction(index: &mut usize, chunk: &CompileTimeChunk, ir: &IntermediateCode) {
         let code = &chunk.code;
 
-        match Instruction::try_from(code[*index]) {
-            Ok(instruction) => {
-                let instruction_name = format!("{:?}", instruction).to_lowercase();
-                let padded_instruction = format!("{:<15}", instruction_name);
-                let instr_index = format!("{:03}", index);
+        if let Ok(instruction) = Instruction::try_from(code[*index]) {
+            let instruction_name = format!("{instruction:?}").to_lowercase();
+            let padded_instruction = format!("{instruction_name:<15}");
+            let instr_index = format!("{index:03}");
 
-                match instruction {
-                    Instruction::Push => {
-                        let pos = Decompiler::combine_u8_to_u16(code[*index + 1], code[*index + 2]);
-                        println!(
-                            "{}  {} {}   ; {:?}",
-                            instr_index, padded_instruction, pos, chunk.constants[pos as usize]
-                        );
-                        *index += 2;
-                    }
-                    Instruction::GetLocal
-                    | Instruction::SetLocal
-                    | Instruction::IncrementLocal
-                    | Instruction::DecrementLocal => {
-                        println!(
-                            "{}  {} {}",
-                            instr_index,
-                            padded_instruction,
-                            code[*index + 1]
-                        );
-                        *index += 1;
-                    }
+            match instruction {
+                Instruction::Push => {
+                    let pos = Decompiler::combine_u8_to_u16(code[*index + 1], code[*index + 2]);
+                    println!(
+                        "{}  {} {}   ; {:?}",
+                        instr_index, padded_instruction, pos, chunk.constants[pos as usize]
+                    );
+                    *index += 2;
+                }
+                Instruction::GetLocal
+                | Instruction::SetLocal
+                | Instruction::IncrementLocal
+                | Instruction::DecrementLocal => {
+                    println!(
+                        "{}  {} {}",
+                        instr_index,
+                        padded_instruction,
+                        code[*index + 1]
+                    );
+                    *index += 1;
+                }
 
-                    Instruction::Call => {
-                        let function_index = Decompiler::combine_u8_to_u32(
-                            code[*index + 1],
-                            code[*index + 2],
-                            code[*index + 3],
-                            code[*index + 4],
-                        );
+                Instruction::Call => {
+                    let function_index = Decompiler::combine_u8_to_u32(
+                        code[*index + 1],
+                        code[*index + 2],
+                        code[*index + 3],
+                        code[*index + 4],
+                    );
 
-                        let arity = code[*index + 5];
+                    let arity = code[*index + 5];
 
-                        println!(
-                            "{}  {} {}   ; {} ({} args)",
-                            instr_index,
-                            padded_instruction,
-                            function_index,
-                            ir.graph.inverse_connections[&function_index],
-                            arity
-                        );
-                        *index += 5;
-                    }
+                    println!(
+                        "{}  {} {}   ; {} ({} args)",
+                        instr_index,
+                        padded_instruction,
+                        function_index,
+                        ir.graph.inverse_connections[&function_index],
+                        arity
+                    );
+                    *index += 5;
+                }
 
-                    Instruction::InvokeVirtual => {
-                        let vtable_index = Decompiler::combine_u8_to_u32(
-                            code[*index + 1],
-                            code[*index + 2],
-                            code[*index + 3],
-                            code[*index + 4],
-                        );
+                Instruction::InvokeVirtual => {
+                    let vtable_index = Decompiler::combine_u8_to_u32(
+                        code[*index + 1],
+                        code[*index + 2],
+                        code[*index + 3],
+                        code[*index + 4],
+                    );
 
-                        let function_index = Decompiler::combine_u8_to_u32(
-                            code[*index + 5],
-                            code[*index + 6],
-                            code[*index + 7],
-                            code[*index + 8],
-                        );
+                    let function_index = Decompiler::combine_u8_to_u32(
+                        code[*index + 5],
+                        code[*index + 6],
+                        code[*index + 7],
+                        code[*index + 8],
+                    );
 
-                        let arity = code[*index + 9];
+                    let arity = code[*index + 9];
 
-                        println!(
-                            "{instr_index}  {padded_instruction} V{vtable_index} Fn{function_index}   ; ({arity} args)"
-                        );
+                    println!(
+                        "{instr_index}  {padded_instruction} V{vtable_index} Fn{function_index}   ; ({arity} args)"
+                    );
 
-                        *index += 9;
-                    }
+                    *index += 9;
+                }
 
-                    Instruction::InvokeNative => {
-                        let function_index = Decompiler::combine_u8_to_u32(
-                            code[*index + 1],
-                            code[*index + 2],
-                            code[*index + 3],
-                            code[*index + 4],
-                        );
+                Instruction::InvokeNative => {
+                    let function_index = Decompiler::combine_u8_to_u32(
+                        code[*index + 1],
+                        code[*index + 2],
+                        code[*index + 3],
+                        code[*index + 4],
+                    );
 
-                        let arity = code[*index + 5];
+                    let arity = code[*index + 5];
 
-                        println!(
-                            "{instr_index}  {padded_instruction} {function_index}   ; ({arity} args)"
-                        );
-                        *index += 5;
-                    }
+                    println!(
+                        "{instr_index}  {padded_instruction} {function_index}   ; ({arity} args)"
+                    );
+                    *index += 5;
+                }
 
-                    Instruction::New => {
-                        let arity = code[*index + 1];
+                Instruction::New => {
+                    let arity = code[*index + 1];
 
-                        println!("{instr_index}  {padded_instruction} {arity}");
-                        *index += 1;
-                    }
+                    println!("{instr_index}  {padded_instruction} {arity}");
+                    *index += 1;
+                }
 
-                    Instruction::GetField | Instruction::SetField | Instruction::Array => {
-                        let field_index = code[*index + 1];
+                Instruction::GetField | Instruction::SetField | Instruction::Array => {
+                    let field_index = code[*index + 1];
 
-                        println!("{instr_index}  {padded_instruction} {field_index}");
-                        *index += 1;
-                    }
+                    println!("{instr_index}  {padded_instruction} {field_index}");
+                    *index += 1;
+                }
 
-                    Instruction::JFalse
-                    | Instruction::Jump
-                    | Instruction::Loop
-                    | Instruction::JTrue => {
-                        let offset = Decompiler::combine_u8_to_u32(
-                            code[*index + 1],
-                            code[*index + 2],
-                            code[*index + 3],
-                            code[*index + 4],
-                        );
+                Instruction::JFalse
+                | Instruction::Jump
+                | Instruction::Loop
+                | Instruction::JTrue => {
+                    let offset = Decompiler::combine_u8_to_u32(
+                        code[*index + 1],
+                        code[*index + 2],
+                        code[*index + 3],
+                        code[*index + 4],
+                    );
 
-                        println!("{instr_index}  {padded_instruction}     ; {offset}");
-                        *index += 4;
-                    }
-                    _ => {
-                        println!("{instr_index}  {padded_instruction}");
-                    }
+                    println!("{instr_index}  {padded_instruction}     ; {offset}");
+                    *index += 4;
+                }
+                _ => {
+                    println!("{instr_index}  {padded_instruction}");
                 }
             }
-            Err(_) => {}
         }
 
         *index += 1;
